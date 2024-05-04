d8cbda194cba1d3582a3e6cb94206133e75fbb8d
heller77 <koki001230@icloud.com>
2024-05-04 14:30:31 +0900

    chapter1修正

--- chapter1/chapter1_main.js
+++ chapter1/chapter1_main.js
@@ -2,14 +2,15 @@
 window.addEventListener("load", chapter1_main);
 
 function chapter1_main() {
-    //canvasを取得
-    const canvas = document.getElementById("targetCanvas");
-    //canvasに付いてるwebgl(オブジェクト？)を取得
-    const gl = canvas.getContext("webgl");
+  //canvasを取得
+  const canvas = document.getElementById("targetCanvas");
 
-    //塗りつぶす色を指定(alphaを0にすると透明になるので注意！)
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
-    //画面を塗りつぶす
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  //canvasに付いてるwebglコンテキストを取得（初期化）
+  //glオブジェクトの関数を呼ぶことで描画出来る
+  const gl = canvas.getContext("webgl");
 
+  //塗りつぶす色を指定(alphaを0にすると透明になるので注意！)
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
+  //画面を塗りつぶす
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
